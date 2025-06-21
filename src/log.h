a7b0c72047cf7a59bd9afc6a417bd04aadb9ecb8
maghiorldon <maggioeldon@my.jccmi.edu>
2025-06-21 08:58:23 +0800

    Update log.h

--- src/log.h
+++ src/log.h
@@ -1,9 +1,12 @@
+// src/log.h - 訓練過程輸出模擬介面
+
 #ifndef LOG_H
 #define LOG_H
 
-#include <cstdio>
+#include "gpu_model.h"
 
-void log_info(const char* fmt, ...);
-void log_error(const char* fmt, ...);
+void log_device_info();
+void log_training_step(int step);
+void log_shard_sync(const Result& r, int step);
 
-#endif
+#endif // LOG_H
